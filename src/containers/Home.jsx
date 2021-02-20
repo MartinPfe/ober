16ab4d06528003cea29929f983504cee8b25fdd9
Martin <martin.pfe@gmail.com>
2021-02-20 12:49:44 -0300

    Agrego style para probar

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <figure className="bg-gray-100 rounded-xl p-8">
+      <div className="pt-6 text-center space-y-4">
+        <blockquote>
+          <p className="text-lg font-semibold">Ober APP - WIP!</p>
+        </blockquote>
+        <figcaption className="font-medium">
+          <div className="text-cyan-600">Martín Pfeiffer</div>
+          <div className="text-gray-500">Argentina</div>
+        </figcaption>
+      </div>
+    </figure>
+  );
 };
 
 export default Home;

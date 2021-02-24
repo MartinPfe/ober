439d17f0d3988eb178a8ab8aba7d6cc615dadcd4
Martin <martin.pfe@gmail.com>
2021-02-23 23:02:28 -0300

    Agrego layout

---
+++ src/containers/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <a
+        href="https://cafecito.app/fifer"
+        rel="noreferrer noopener"
+        target="_blank"
+      >
+        <img
+          srcset="https://cdn.cafecito.app/imgs/buttons/button_4.png 1x, https://cdn.cafecito.app/imgs/buttons/button_4_2x.png 2x, https://cdn.cafecito.app/imgs/buttons/button_4_3.75x.png 3.75x"
+          src="https://cdn.cafecito.app/imgs/buttons/button_4.png"
+          alt="Invitame un café en cafecito.app"
+        />
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Home, Restaurants } from "../containers";
+import Layout from "../containers/Layout";
 
 const App = () => {
   return (
     <BrowserRouter>
       {/* <AppContext.Provider value={initialState}> */}
-      {/* <Layout> */}
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/restaurants" component={Restaurants} />
-      </Switch>
-      {/* </Layout>
-      </AppContext.Provider> */}
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/restaurants" component={Restaurants} />
+        </Switch>
+      </Layout>
+      {/* </AppContext.Provider> */}
     </BrowserRouter>
   );
 };

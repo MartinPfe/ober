ea53e79cc57a3f64f4acf6813901820b25d7ab7e
Martin <martin.pfe@gmail.com>
2021-02-28 18:19:34 -0300

    Fix

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,17 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 
-console.log("REACT_APP_API_KEY:", process.env.REACT_APP_API_KEY);
-console.log("REACT_APP_AUTH_DOMAIN:", process.env.REACT_APP_AUTH_DOMAIN);
-console.log("REACT_APP_PROJECT_ID:", process.env.REACT_APP_PROJECT_ID);
-console.log("REACT_APP_STORAGE_BUCKET:", process.env.REACT_APP_STORAGE_BUCKET);
-console.log(
-  "REACT_APP_MESSAGING_SENDER_ID:",
-  process.env.REACT_APP_MESSAGING_SENDER_ID
-);
-console.log("REACT_APP_APP_ID:", process.env.REACT_APP_APP_ID);
-console.log("REACT_APP_MEASUREMENT_ID:", process.env.REACT_APP_MEASUREMENT_ID);
-
 firebase.initializeApp({
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,

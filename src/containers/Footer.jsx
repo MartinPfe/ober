ce704db6c04ae85f115b1c27905b8ce6ea0abd4c
Martin <martin.pfe@gmail.com>
2021-02-28 21:41:55 -0300

    Saco cafecito

--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -1,21 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return (
-    <div>
-      <a
-        href="https://cafecito.app/fifer"
-        rel="noreferrer noopener"
-        target="_blank"
-      >
-        <img
-          srcSet="https://cdn.cafecito.app/imgs/buttons/button_4.png 1x, https://cdn.cafecito.app/imgs/buttons/button_4_2x.png 2x, https://cdn.cafecito.app/imgs/buttons/button_4_3.75x.png 3.75x"
-          src="https://cdn.cafecito.app/imgs/buttons/button_4.png"
-          alt="Invitame un café en cafecito.app"
-        />
-      </a>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Footer;
